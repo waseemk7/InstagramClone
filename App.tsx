b3e053d7942e4ff16d089d63f8f434724f982caf
Waseem Kurne <kurne.waseem@gmail.com>
2023-02-03 20:29:03 +0530

    tab bar icons

--- App.tsx
+++ App.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Feather from 'react-native-vector-icons/Feather';
+import Foundation from 'react-native-vector-icons/Foundation';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import IoniconsIcon from 'react-native-vector-icons/Ionicons';
 
 import HomeScreen from './src/screens/HomeScreen';
 import DiscoveryScreen from './src/screens/DiscoveryScreen';
@@ -13,7 +17,31 @@ const Tab = createBottomTabNavigator();
 function App(): JSX.Element {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({route}) => ({
+          tabBarIcon: ({focused, color, size}) => {
+            if (route.name === 'Home') {
+              return <Foundation name="home" size={size} color={color} />;
+            }
+            if (route.name === 'Discovery') {
+              return <Feather name="search" size={size} color={color} />;
+            }
+            if (route.name === 'Post') {
+              return <Feather name="plus-square" size={size} color={color} />;
+            }
+            if (route.name === 'Notifications') {
+              return <AntDesign name="hearto" size={size} color={color} />;
+            }
+            if (route.name === 'Profile') {
+              return (
+                <IoniconsIcon name="person-outline" size={size} color={color} />
+              );
+            }
+          },
+          tabBarActiveTintColor: '#000000',
+          tabBarInactiveTintColor: 'gray',
+          tabBarShowLabel: false,
+        })}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Discovery" component={DiscoveryScreen} />
         <Tab.Screen name="Post" component={CreatePostScreen} />
