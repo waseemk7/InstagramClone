1bc68499e80a920e1226bc75181a1a128cd5beeb
Waseem Kurne <kurne.waseem@gmail.com>
2023-02-03 18:50:31 +0530

    like button

--- src/components/Post/components/Footer/index.tsx
+++ src/components/Post/components/Footer/index.tsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View} from 'react-native';
 import ADIcon from 'react-native-vector-icons/AntDesign';
 import FontistoIcon from 'react-native-vector-icons/Fontisto';
 import IoniconsIcon from 'react-native-vector-icons/Ionicons';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
 import styles from './styles';
+import {TouchableWithoutFeedback} from 'react-native';
 
 const Footer = ({likesCount, caption, postedAt}) => {
+  const [isLiked, setIsLiked] = useState<boolean>(false);
+
+  const onLikedPressed = () => {
+    setIsLiked(!isLiked);
+  };
+
   return (
     <View style={styles.conatiner}>
       <View style={styles.iconsContainer}>
         <View style={styles.leftIcons}>
-          <ADIcon name="hearto" size={25} color={'#545454'} />
+          <TouchableWithoutFeedback onPress={onLikedPressed}>
+            {isLiked ? (
+              <ADIcon name="heart" size={25} color={'#e73838'} />
+            ) : (
+              <ADIcon name="hearto" size={25} color={'#545454'} />
+            )}
+          </TouchableWithoutFeedback>
           <FontistoIcon name="comment" size={23} color={'#545454'} />
           <IoniconsIcon
             name="paper-plane-outline"
