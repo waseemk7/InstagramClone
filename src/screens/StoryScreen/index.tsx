c9ae0fd89435c902559cf3cae30b49527ec1271b
Waseem Kurne <kurne.waseem@gmail.com>
2023-02-13 13:34:45 +0530

    profile picture and send message on story screen

--- src/screens/StoryScreen/index.tsx
+++ src/screens/StoryScreen/index.tsx
@@ -1,12 +1,18 @@
-import {useRoute} from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 import React, {useEffect, useState} from 'react';
 import {
+  View,
+  Text,
   SafeAreaView,
   ImageBackground,
   ActivityIndicator,
   TouchableWithoutFeedback,
   Dimensions,
+  TextInput,
 } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import IoniconsIcon from 'react-native-vector-icons/Ionicons';
+import ProfilePicture from '../../components/ProfilePicture';
 import storiesData from '../../data/stories';
 
 import styles from './styles';
@@ -14,10 +20,12 @@ import styles from './styles';
 const StoryScreen = () => {
   const [userStories, setUserStories] = useState(null);
   const [activeStoryIndex, setActiveStoryIndex] = useState(null);
+
   const route = useRoute();
+  const navigation = useNavigation();
+  const userId = route.params.userId;
 
   useEffect(() => {
-    const userId = route.params.userId;
     const userStories = storiesData.find(
       storyData => storyData.user.id === userId,
     );
@@ -39,8 +47,17 @@ const StoryScreen = () => {
     }
   }, [activeStoryIndex]);
 
+  const navigateToNextUser = () => {
+    navigation.push('Story', {userId: (parseInt(userId) + 1).toString()});
+  };
+
+  const navigateToPrevUser = () => {
+    navigation.push('Story', {userId: (parseInt(userId) - 1).toString()});
+  };
+
   const handleNextStory = () => {
     if (activeStoryIndex >= userStories.stories.length - 1) {
+      navigateToNextUser();
       return;
     }
     setActiveStoryIndex(activeStoryIndex + 1);
@@ -48,6 +65,7 @@ const StoryScreen = () => {
 
   const handlePrevStory = () => {
     if (activeStoryIndex <= 0) {
+      navigateToPrevUser();
       return;
     }
     setActiveStoryIndex(activeStoryIndex - 1);
@@ -78,7 +96,33 @@ const StoryScreen = () => {
       <TouchableWithoutFeedback onPress={handlePress}>
         <ImageBackground
           source={{uri: activeStory.imageUri}}
-          style={styles.image}></ImageBackground>
+          style={styles.image}>
+          <View style={styles.userInfo}>
+            <ProfilePicture uri={activeStory.imageUri} size={50} />
+            <Text style={styles.userName}>{userStories.user.name}</Text>
+            <Text style={styles.postedTime}>{activeStory.postedTime}</Text>
+          </View>
+          <View style={styles.bottomContainer}>
+            <View style={styles.cameraButton}>
+              <Feather name="camera" size={30} color={'#ffffff'} />
+            </View>
+            <View style={styles.textInputContainer}>
+              <TextInput
+                style={styles.textInput}
+                editable
+                placeholder="Send message"
+                placeholderTextColor={'white'}
+              />
+            </View>
+            <View style={styles.messageButton}>
+              <IoniconsIcon
+                name="paper-plane-outline"
+                size={35}
+                color={'#ffffff'}
+              />
+            </View>
+          </View>
+        </ImageBackground>
       </TouchableWithoutFeedback>
     </SafeAreaView>
   );
--- src/screens/StoryScreen/styles.ts
+++ src/screens/StoryScreen/styles.ts
@@ -7,7 +7,56 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     resizeMode: 'cover',
+    justifyContent: 'space-between',
+  },
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  userName: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 18,
+  },
+  postedTime: {
+    marginLeft: 10,
+    fontWeight: '700',
+    color: '#808080',
+    fontSize: 16,
+  },
+  bottomContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+    marginHorizontal: 10,
+  },
+  cameraButton: {
+    width: 50,
+    alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'white',
+    marginHorizontal: 10,
+    borderRadius: 50,
+  },
+  messageButton: {
+    width: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    height: '100%',
+    color: 'white',
+    fontSize: 16,
+  },
+  textInputContainer: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: 'white',
+    marginHorizontal: 10,
+    paddingHorizontal: 10,
+    borderRadius: 50,
+    height: 50,
   },
 });
 
